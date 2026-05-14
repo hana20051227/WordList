efb5be2c4a4ca3180648fdc1ba2d2b74a1546ebd
Hana Tokumitsu <nishinokana87326@gmail.com>
2026-05-15 01:14:00 +0900

    Initial Commit

---
+++ WordList/WordListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordListApp.swift
+//  WordList
+//
+//  Created by 徳光はな on 2026/05/15.
+//
+
+import SwiftUI
+
+@main
+struct WordListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
